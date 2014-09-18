31fd9bd43b00e4816b07ab3dc3a5352928c3dc9e
Dan Smith <dan.smith@chrometoaster.com>
2014-09-18 14:39:47 +1200

    Fix detach/append issue (bug #1)

--- dev/ct-jquery-last-child.js
+++ dev/ct-jquery-last-child.js
@@ -2,19 +2,19 @@
 
 ( function($) {
 
-    var $body = $('body');
-    var $container = $('.polyfill'); // this must be present in the page, wrapping the content you wish to polyfill
+    var $container = $('.polyfill'); // this must be present in the page, wrapping the .polyfill--contents you wish to polyfill
+    var $contents = $('.polyfill--contents');
     var parent_selector = '[class^="l-"], [class*=" l-"]';
     var child_selector = '.colgroup, .col, [class^="lc-"], [class*=" lc-"]';
     var polyfill_class = 'last-child';
 
-    if ( ! $body.length || ! $container.length ) {
+    if ( ! $container.length || ! $contents.length ) {
         return;
     }
 
     // detach container from DOM for heavy manipulation of contents
     // http://flippinawesome.org/2013/11/25/writing-better-jquery-code/
-    $container.detach();
+    $contents.detach();
 
     // LAST-CHILD
     // http://jsfiddle.net/dotherightthing/yLUMf/
@@ -22,7 +22,7 @@
     // as layout containers may use several classes,
     // we check for both a selector starting with 'l-'
     // and a selector containing 'l-'
-    var $layouts = $container.find( parent_selector );
+    var $layouts = $contents.find( parent_selector );
 
     // Use setTimeout to avoid script timeouts / crashes
     $layouts.each( function(i, item) {
@@ -45,6 +45,6 @@
     });
 
     // reattach container to DOM
-    $body.append( $container );
+    $container.append( $contents );
 
 })(jQuery);
